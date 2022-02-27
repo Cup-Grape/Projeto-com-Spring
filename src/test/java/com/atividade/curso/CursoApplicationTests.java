0726aa02076c728b46385f171f5bd30117c627fe
Cup-Grape <feliposs11@gmail.con>
2022-02-27 14:50:08 -0300

    primeiro commit
    Commit feito utilizando o GitKraken

---
+++ src/test/java/com/atividade/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
