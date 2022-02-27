e7de3958e7af207918e2b746f97c1137c03605ac
Cup-Grape <feliposs11@gmail.con>
2022-02-27 15:18:12 -0300

    User entity and resource
    Foi implementado no projeto os seguintes componentes:
    
    * Basic attributes
    * Constructors
    * Getters & Setters
    * HashCode & equals
    * Serializable

---
+++ src/main/java/com/atividade/curso/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.atividade.curso.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atividade.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"João","joao@gmai.com", "1192545414", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
